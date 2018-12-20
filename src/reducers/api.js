c6ac2141329b0530a6edb8e70438752e03f8123c
Andrey Kozlov <l-2732@yandex.ru>
2018-12-20 06:01:07 +0000

    switch to upstream, close #33

--- src/reducers/api.js
+++
@@ -1,9 +0,0 @@
-import preview from '../preview.json';
-export async function loadData() {
-  const data = await (await fetch('/data.json')).json();
-  return data;
-}
-
-export async function loadPreviewData() {
-  return preview;
-}
